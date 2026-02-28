ed493122b93b445ed67a4e2ceec2c70f57c5a5fa
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-02-28 14:14:53 -0500

    Package changes

--- src/main/java/dev/simplified/collection/concurrent/atomic/AtomicMap.java
+++ src/main/java/dev/simplified/collection/concurrent/atomic/AtomicMap.java
@@ -1,8 +1,8 @@
 package dev.sbs.api.collection.concurrent.atomic;
 
 import dev.sbs.api.collection.concurrent.iterator.ConcurrentIterator;
+import dev.sbs.api.collection.mutable.MutableBoolean;
 import dev.sbs.api.collection.search.Searchable;
-import dev.sbs.api.mutable.MutableBoolean;
 import dev.sbs.api.stream.StreamUtil;
 import dev.sbs.api.stream.pair.PairStream;
 import dev.sbs.api.util.NumberUtil;
